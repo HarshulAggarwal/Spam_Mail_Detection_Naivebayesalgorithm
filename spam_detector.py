b3079f31f36c0a570414e60afc18c0a4a1d7ad57
Harshul Aggarwal <141482788+HarshulAggarwal@users.noreply.github.com>
2024-04-21 23:22:14 +0530

    Update spam_detector.py

--- spam_detector.py
+++ spam_detector.py
@@ -23,8 +23,8 @@ def transform_text(text):
     for i in text:
         y.append(lemma.lemmatize(i))
     return " ".join(y)
-tfidf = pickle.load(open(r'C:\Users\harsh\Projects\SpamMailDetection\vectorizer.pkl','rb'))
-model = pickle.load(open(r'C:\Users\harsh\Projects\SpamMailDetection\model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
